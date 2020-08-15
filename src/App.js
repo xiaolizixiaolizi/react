42c638f93e034e485c26f659c3446599ff9031c6
xiaolizi <1756329434@qq.com>
2020-08-15 21:35:07 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './App.css';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+      <div>
+        <h2>hello react</h2>
+      </div>
+    );
+  }
+}
+
---
+++ src/HelloReact.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react' //imrc
+class App extends Component {  //ccc
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+      <div>
+        <h2>hello world</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
