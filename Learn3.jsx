654332cb3c7167ec215d219b15eff50aa925b4b0
xiaolizi <1756329434@qq.com>
2020-08-15 21:57:41 +0800

    commit learn3

---
+++ Learn3.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+class Learn3 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+      <div>
+        <h3>111</h3>
+      </div>
+    );
+  }
+}
+
+export default Learn3;
