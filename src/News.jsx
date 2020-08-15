f7afbc47ec906837f819ace57298eae025fdb9af
xiaolizi <1756329434@qq.com>
2020-08-15 21:50:05 +0800

    learn-bendi-2

---
+++ src/News.jsx
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+      <div>111</div>
+    );
+  }
+}
+
+export default News;
