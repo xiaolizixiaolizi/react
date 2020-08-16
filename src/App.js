d9d09c0209139c785d5c3983e87db648585ddcc1
xiaolizi <1756329434@qq.com>
2020-08-16 10:41:27 +0800

    clear learn01

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-import './App.css';
 export default class App extends Component {
   constructor(props) {
     super(props);
--- src/learn13.jsx
+++
@@ -1,3 +0,0 @@
-import React, { Component } from 'react';
-console.log(1);
-console.log(2);
